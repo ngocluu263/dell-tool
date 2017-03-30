78f8274c28f51b8be644c240be5a72e2ebddd9d6
luunguyen <luunguyen@ccintegration.com>
2017-03-30 17:08:21 +0700

    Update

--- dmidecode.py
+++ dmidecode.py
@@ -219,5 +219,10 @@ def _show(info):
     print ' DVD/HardDisk:'
     print (os.system('sudo lshw -class disk -short'))
 
+
+print ' =================================================== \n'
+print ' Network:'
+print (os.system('sudo lshw -class network -short'))
+
 if __name__ == '__main__':
     profile()
