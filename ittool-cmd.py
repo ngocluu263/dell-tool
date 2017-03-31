24f1e21d47cbcaa373c5122326af47fb2cc8e7bc
luunguyen <luunguyen@ccintegration.com>
2017-03-31 17:01:22 +0700

    Update

--- ittool-cmd.py
+++ ittool-cmd.py
@@ -137,7 +137,7 @@ def _show(info):
         i, unit = mem['Size'].split()
         cnt += 1
         total += int(i)
-        print (" + Manufacturer: %s, Type: %s Speed:%s, Size:%s" % (mem['Manufacturer'], mem['Type'], mem['Speed'], mem['Size']))
+        print (" + Manufacturer: %s, Type: %s Speed: %s, Size: %s" % (mem['Manufacturer'], mem['Type'], mem['Speed'], mem['Size']))
     print ' + %d memory stick(s), ' \
           '\n + %d %s in total' % (
         cnt,
