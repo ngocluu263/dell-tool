7ddabd88e1f4c8dcc276fcf07de2ed74a9544015
luunguyen <luunguyen@ccintegration.com>
2017-03-31 14:00:27 +0700

    Update

--- lshw.py
+++ lshw.py
@@ -1,30 +1,5 @@
-#!/usr/bin/python -tt
-# Parse lshw output and collect hardware data.
+import subprocess
 
-# Code is taken from https://gist.github.com/amitsaha/4554484
-# I need to modify and pick various other part such as cpu, memory.
-
-
-from lxml import etree
-from subprocess import Popen, PIPE
-
-inventory = Popen(['lshw', '-xml', '-numeric'], stdout=PIPE).communicate()[0]
-inventory = etree.XML(inventory)
-
-find_disks = etree.XPath(".//node[@class='disk']")
-
-numdisks = 0
-diskspace = 0
-for disk in find_disks(inventory):
-    # has to be a hard-disk
-    if disk.find('size') is not None:
-        numdisks = numdisks + 1
-        diskspace = diskspace + int(disk.find('size').text)
-        print disk.find('description').text, disk.find('product').text, disk.find('logicalname').text
-        print 'Disk Space: ', disk.find('size').text
-        print 'Sector size: ', disk.find('configuration/setting/[@id="sectorsize"]').get('value')
-        print
-
-print 'Num disks', numdisks
-print 'Total disk space', diskspace / (1024 ** 2)
-
+result_text = subprocess.Popen("sudo lshw -class network -short",
+                               stdout=subprocess.PIPE, shell=True).communicate()[0]
+print result_text
